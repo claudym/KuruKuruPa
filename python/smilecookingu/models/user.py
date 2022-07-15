f8a09b3d084ed2bbb0eb2a187722bcf68b005d2f
claudym <claudy.m.mejia@gmail.com>
2022-07-15 12:16:08 -0400

    changed "db.model" to "db.Model"

--- python/smilecookingu/models/user.py
+++ python/smilecookingu/models/user.py
@@ -1,7 +1,7 @@
 from extensions import db
 
 
-class User(db.model):
+class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False, unique=True)
