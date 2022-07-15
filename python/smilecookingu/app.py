2ebe446c57b4d582cf5113f9f44ed3f0751af745
claudym <claudy.m.mejia@gmail.com>
2022-07-15 17:03:34 -0400

    added user resource routing in app.py

--- python/smilecookingu/app.py
+++ python/smilecookingu/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_restful import Api
 from flask_migrate import Migrate
 from extensions import db
-from models.user import User
+from resources.user import UserListResource
 from resources.recipe import RecipeListResource, RecipeResource, RecipePublishResource
 
 
@@ -24,6 +24,7 @@ def register_resources(app):
     api.add_resource(RecipeListResource, '/recipes')
     api.add_resource(RecipeResource, '/recipes/<int:recipe_id>')
     api.add_resource(RecipePublishResource, '/recipes/<int:recipe_id>/publish')
+    api.add_resource(UserListResource, '/users')
 
 
 if __name__ == '__main__':
