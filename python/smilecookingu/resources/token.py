46f5761950988ee1f129e2c9e264bcaa6f920728
claudym <claudy.m.mejia@gmail.com>
2022-07-19 10:41:20 -0400

    added refresh token functionality and RefreshResource class in the token resource

--- python/smilecookingu/resources/token.py
+++ python/smilecookingu/resources/token.py
@@ -1,7 +1,12 @@
 from http import HTTPStatus
 from flask import request
 from flask_restful import Resource
-from flask_jwt_extended import create_access_token
+from flask_jwt_extended import (
+    create_access_token,
+    create_refresh_token,
+    jwt_required,
+    get_jwt_identity
+)
 from utils import check_password
 from models.user import User
 
@@ -12,8 +17,17 @@ class TokenResource(Resource):
         email = json_data.get('email')
         password = json_data.get('password')
         user = User.get_by_email(email=email)
-
         if user is None or not check_password(password, user.password):
             return {'message': 'email or password is incorrect'}, HTTPStatus.UNAUTHORIZED
-        access_token = create_access_token(identity=user.id)
+
+        access_token = create_access_token(identity=user.id, fresh=True)
+        refresh_token = create_refresh_token(identity=user.id)
+        return {'access_token': access_token, 'refresh_token': refresh_token}, HTTPStatus.OK
+
+
+class RefreshResource(Resource):
+    @jwt_required(refresh=True)
+    def post(self):
+        current_user = get_jwt_identity()
+        access_token = create_access_token(identity=current_user, fresh=False)
         return {'access_token': access_token}, HTTPStatus.OK
