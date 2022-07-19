5d88f41a8af77677878b517f24bf32cd99f5dd56
claudym <claudy.m.mejia@gmail.com>
2022-07-19 10:44:28 -0400

    added route /refresh for RefreshResource in app.py

--- python/smilecookingu/app.py
+++ python/smilecookingu/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_restful import Api
 from flask_migrate import Migrate
 from extensions import db, jwt
-from resources.token import TokenResource
+from resources.token import TokenResource, RefreshResource
 from resources.user import UserListResource, UserResource, MeResource
 from resources.recipe import RecipeListResource, RecipeResource, RecipePublishResource
 
@@ -30,6 +30,7 @@ def register_resources(app):
     api.add_resource(UserResource, '/users/<string:username>')
     api.add_resource(MeResource, '/me')
     api.add_resource(TokenResource, '/token')
+    api.add_resource(RefreshResource, '/refresh')
 
 
 if __name__ == '__main__':
