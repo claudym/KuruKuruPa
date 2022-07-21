40ecec7838e04c87d0397aae24fd630f01bc5f6a
claudym <claudy.m.mejia@gmail.com>
2022-07-21 17:09:08 -0400

    modified the user resource so to use marshmallow

--- python/smilecookingu/resources/user.py
+++ python/smilecookingu/resources/user.py
@@ -2,34 +2,27 @@ from flask import request
 from flask_restful import Resource
 from http import HTTPStatus
 from flask_jwt_extended import jwt_required, get_jwt_identity
-from utils import hash_password
 from models.user import User
+from schemas.user import UserSchema
+
+user_schema = UserSchema()
+user_public_schema = UserSchema(exclude=('email', ))
 
 
 class UserListResource(Resource):
     def post(self):
         json_data = request.get_json()
-        username = json_data.get('username')
-        email = json_data.get('email')
-        non_hash_password = json_data.get('password')
-
-        if User.get_by_username(username):
+        data, errors = user_schema.load(data=json_data)
+        if errors:
+            return {'message': 'Validation errors', 'errors': 'errors'}, HTTPStatus.BAD_REQUEST
+        if User.get_by_username(data.get('username')):
             return {'message': 'username already used'}, HTTPStatus.BAD_REQUEST
-        if User.get_by_username(email):
+        if User.get_by_username(data.get('email')):
             return {'message': 'email already used'}, HTTPStatus.BAD_REQUEST
 
-        password = hash_password(non_hash_password)
-        user = User(
-            username=username,
-            email=email,
-            password=password
-        )
+        user = User(**data)
         user.save()
-        data = {
-            'id': user.id,
-            'username': user.username,
-            'email': user.email
-        }
+        data = user_schema.dump(user)
         return data, HTTPStatus.CREATED
 
 
@@ -42,16 +35,9 @@ class UserResource(Resource):
 
         current_user = get_jwt_identity()
         if current_user == user.id:
-            data = {
-                'id': user.id,
-                'username': user.username,
-                'email': user.email
-            }
+            data = user_schema.dump(user)
         else:
-            data = {
-                'id': user.id,
-                'username': user.username
-            }
+            data = user_public_schema.dump(user)
         return data, HTTPStatus.OK
 
 
@@ -59,9 +45,5 @@ class MeResource(Resource):
     @jwt_required()
     def get(self):
         user = User.get_by_id(id=get_jwt_identity())
-        data = {
-            'id': user.id,
-            'username': user.username,
-            'email': user.email
-        }
+        data = user_schema.dump(user)
         return data, HTTPStatus.OK
