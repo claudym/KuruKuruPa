b3cd707da8eb210e6845d563a82b83e408b97518
claudym <claudy.m.mejia@gmail.com>
2022-07-22 10:05:44 -0400

    fixed validation in user resource by importing ValidationError from marshmallow and catching it

--- python/smilecookingu/resources/user.py
+++ python/smilecookingu/resources/user.py
@@ -2,6 +2,7 @@ from flask import request
 from flask_restful import Resource
 from http import HTTPStatus
 from flask_jwt_extended import jwt_required, get_jwt_identity
+from marshmallow import ValidationError
 from models.user import User
 from schemas.user import UserSchema
 
@@ -12,9 +13,10 @@ user_public_schema = UserSchema(exclude=('email', ))
 class UserListResource(Resource):
     def post(self):
         json_data = request.get_json()
-        data, errors = user_schema.load(data=json_data)
-        if errors:
-            return {'message': 'Validation errors', 'errors': 'errors'}, HTTPStatus.BAD_REQUEST
+        try:
+            data = user_schema.load(data=json_data)
+        except ValidationError as err:
+            return {'message': 'Validation errors', 'errors': err.messages}, HTTPStatus.BAD_REQUEST
         if User.get_by_username(data.get('username')):
             return {'message': 'username already used'}, HTTPStatus.BAD_REQUEST
         if User.get_by_username(data.get('email')):
