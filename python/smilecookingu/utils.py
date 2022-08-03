21a7f36b63068049e6de733d95f208b9a728e657
claudym <claudy.m.mejia@gmail.com>
2022-08-03 17:05:25 -0400

    implemented image compression using pillow

--- python/smilecookingu/utils.py
+++ python/smilecookingu/utils.py
@@ -1,10 +1,12 @@
+import uuid
+import os
+from PIL import Image
 from passlib.hash import pbkdf2_sha256
 from itsdangerous.url_safe import URLSafeTimedSerializer
 from itsdangerous.exc import BadData
 from flask import current_app
 from flask_uploads import extension
 from extensions import image_set
-import uuid
 
 
 def hash_password(password):
@@ -32,4 +34,26 @@ def verify_token(token, max_age=(30 * 60), salt=None):
 def save_image(image, folder):
     filename = f'{uuid.uuid4()}.{extension(image.filename)}'
     image_set.save(image, folder=folder, name=filename)
+    filename = compress_image(filename=filename, folder=folder)
     return filename
+
+
+def compress_image(filename, folder):
+    file_path = image_set.path(filename=filename, folder=folder)
+    image = Image.open(file_path)
+    if image.mode != 'RGB':
+        image = image.convert('RGB')
+    if max(image.width, image.height) > 1600:
+        maxsize = (1600, 1600)
+        image.thumbnail(maxsize, Image.ANTIALIAS)
+    compressed_filename = f'{uuid.uuid4()}.jpg'
+    compressed_file_path = image_set.path(filename=compressed_filename, folder=folder)
+    image.save(compressed_file_path, optimize=True, quality=85)
+
+    original_size = os.stat(file_path).st_size
+    compressed_size = os.stat(compressed_file_path).st_size
+    percentage = round((original_size-compressed_size) / original_size * 100)
+    print(f'The file size is reduced by {percentage}%, from {original_size} to {compressed_size}.')
+
+    os.remove(file_path)
+    return compressed_filename
