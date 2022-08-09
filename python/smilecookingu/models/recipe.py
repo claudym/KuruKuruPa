6608434abedaea9cf100273081817b53a2b183a7
claudym <claudy.m.mejia@gmail.com>
2022-08-09 10:31:13 -0400

    edited get_all_publish method in the recipe model in order to fit the pagination schema

--- python/smilecookingu/models/recipe.py
+++ python/smilecookingu/models/recipe.py
@@ -1,4 +1,5 @@
 from extensions import db
+from sqlalchemy import asc, desc
 
 
 class Recipe(db.Model):
@@ -25,8 +26,9 @@ class Recipe(db.Model):
             return cls.query.filter_by(user_id=user_id).all()
 
     @classmethod
-    def get_all_published(cls):
-        return cls.query.filter_by(is_publish=True).all()
+    def get_all_published(cls, page, per_page):
+        return cls.query.filter_by(is_publish=True).order_by(desc(cls.created_at)).paginate(page=page, per_page=per_page)
+        # return cls.query.filter_by(is_publish=True).all()
 
     @classmethod
     def get_by_id(cls, recipe_id):
