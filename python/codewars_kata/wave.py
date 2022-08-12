9b65485851df99bb436e8b004f3beef45ee77f41
claudym <claudy.m.mejia@gmail.com>
2022-08-11 23:40:33 -0400

    all versions wave.py

--- python/codewars_kata/wave.py
+++ python/codewars_kata/wave.py
@@ -1,5 +1,16 @@
 def wave(people):
     return [people[:i] + people[i].upper() + people[i+1:] for i in range(len(people)) if people[i] != ' ']
 
-print(wave('hello'))
+def wave2(people):
+    wave_arr = []
+    temp_people = list(people)
+    for i in range(len(people)):
+        if temp_people[i] != ' ':
+            temp = temp_people[i]
+            temp_people[i] = temp_people[i].upper()
+            wave_arr.append(''.join(temp_people))
+            temp_people[i] = temp
+    return wave_arr
+
+# print(wave('hello'))
         
