7945f07ee64fa5025fba6e0e41adc3abf54c42ce
claudym <claudy.m.mejia@gmail.com>
2022-08-29 19:01:34 -0400

    validation: field-level, object-level, and validators

--- python/imdb-clone/mitegoran/watchlist_app/api/serializers.py
+++ python/imdb-clone/mitegoran/watchlist_app/api/serializers.py
@@ -2,9 +2,15 @@ from rest_framework import serializers
 from watchlist_app.models import Movie
 
 
+# validators
+def name_length(value):
+    if len(value) < 2:
+        raise serializers.ValidationError('Name is too short!')
+
+
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField()
+    name = serializers.CharField(validators=[name_length])
     description = serializers.CharField()
     active = serializers.BooleanField()
 
@@ -17,3 +23,15 @@ class MovieSerializer(serializers.Serializer):
         instance.active = validated_data.get('active', instance.active)
         instance.save()
         return instance
+
+    # field-level validation
+    # def validate_name(self, value):
+    #     if len(value) < 2:
+    #         raise serializers.ValidationError('Name is too short!')
+    #     return value
+
+    # object-level validation
+    def validate(self, data):
+        if data['name'] == data['description']:
+            raise serializers.ValidationError('Name and Description should be different!')
+        return data
--- python/imdb-clone/mitegoran/watchlist_app/api/views.py
+++ python/imdb-clone/mitegoran/watchlist_app/api/views.py
@@ -18,7 +18,7 @@ class MovieListAV(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-        return Response(serializer.errors)
+        return Response(serializer.errors, status=status.HTTP_201_CREATED)
 
 
 class MovieDetailsAV(APIView):
