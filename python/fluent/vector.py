38c0cb2fe2f846a4cb82b0267f5f8407cb1da127
claudym <claudy.m.mejia@gmail.com>
2022-09-13 12:45:24 -0400

    add __repr__, __abs__, __bool__, __add__, __mul__ special methods

--- python/fluent/vector.py
+++ python/fluent/vector.py
@@ -4,7 +4,8 @@ class Vector:
     def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
-    
+
+    # string representation of the object for inspection 
     def __repr__(self):
         return f'Vector({self.x}, {self.y})'
     
@@ -12,8 +13,10 @@ class Vector:
         return hypot(self.x, self.y)
     
     def __bool__(self):
-        return bool(abs(self))
+        # return bool(abs(self))
+        return bool(self.x or self.y)
     
+    # arithmetic operator: add
     def __add__(self, other):
         x = self.x + other.x
         y = self.y + other.y
